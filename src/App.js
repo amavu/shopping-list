349da0cd287c8b09af4ab3e0bdeb7e88254f8071
amavu <amandavu15@gmail.com>
2021-06-26 12:39:55 +0200

    create shopping list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newToDoList = [...toDoList, { text: value, isCompleted: false }];
+    setToDoList(newToDoList);
+    setValue("");
+  };
+
+  const isCompletedHandler = (index) => {
+    const newToDoList = [...toDoList];
+    newToDoList[index].isCompleted = !newToDoList[index].isCompleted;
+    setToDoList(newToDoList);
+  };
+
+  const deleteItem = (deleteIndex) => {
+    const newToDoList = [...toDoList];
+    newToDoList.splice(deleteIndex, 1);
+    setToDoList(newToDoList);
+  };
+
+  const ToDoList = () => {
+    return (
+      <div>
+        {toDoList.map((toDoElement, index) => {
+          return (
+            <div key={index}>
+              <span>{toDoElement.text}</span>
+              <input
+                type="checkbox"
+                checked={toDoElement.isCompleted}
+                onChange={() => isCompletedHandler(index)}
+              ></input>
+              <button type="button" onClick={() => deleteItem(index)}>
+                delete
+              </button>
+              {console.log(toDoList)}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>My Shopping List</h1>
       </header>
+      <form onSubmit={submitHandler}>
+        <input
+          value={value}
+          placeholder="Add item"
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+      <ToDoList />
     </div>
   );
-}
+};
 
 export default App;
